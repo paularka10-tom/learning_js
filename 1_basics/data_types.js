9100a74c33ce813c05796faad428fc973865a9e3
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-23 19:10:23 +0000

    datatypes

---
+++ 1_basics/data_types.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS CODE as newer version
+// alert (3+3) //we are using node js, not browser
+
+let name = "Arka"  //string
+let num = 65   //integer   > 2**53
+let isLoggedin = false  //boolean
+let city;
+
+// integer
+// bigint
+//string
+//boolean=> true or  false
+//null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined)
