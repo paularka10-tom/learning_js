e03b2200aa2d813d396fe768f481a220233ce939
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-25 18:37:10 +0000

    Add conversionOperators.js

--- 1_basics/conversionOperations.js
+++ 1_basics/conversionOperations.js
@@ -1,8 +1,8 @@
 let a = undefined
-console.log(typeof a)
+//console.log(typeof a)
 
 let valueInnum = Number(a)
-console.log(typeof valueInnum)
+//console.log(typeof valueInnum)
 //console.log(valueInnum)
 
 // "33"=>33
@@ -12,12 +12,32 @@ console.log(typeof valueInnum)
 
 let isLoggedin = ""
 let bool = Boolean(isLoggedin)
-console.log(bool);
+//console.log(bool);
 
 //1 => true  0=> false
 //""=> false "value" => true
 
 let b= 78
 let word = String(b)
-console.log(word);
-console.log(typeof(word));
+//console.log(word);
+//console.log(typeof(word));
+
+// ************Operations************
+let value= 4
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "GIGGA"
+let str2 = "NIGGA"
+let str3 =str1+ str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1 +"2")
+console.log(1+2+"2")
