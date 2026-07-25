7a6d828f147ee18628281f7e7c1881f38fcf45b4
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-25 17:37:04 +0000

    Add conversionOperators.js

---
+++ 1_basics/conversionOperations.js
@@ -0,0 +1,23 @@
+let a = undefined
+console.log(typeof a)
+
+let valueInnum = Number(a)
+console.log(typeof valueInnum)
+//console.log(valueInnum)
+
+// "33"=>33
+//"33abc"=> NaN
+//true => 1, false => 0
+//undefined => undefined
+
+let isLoggedin = ""
+let bool = Boolean(isLoggedin)
+console.log(bool);
+
+//1 => true  0=> false
+//""=> false "value" => true
+
+let b= 78
+let word = String(b)
+console.log(word);
+console.log(typeof(word));
