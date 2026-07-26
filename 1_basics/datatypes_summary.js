01400e03dd90d311891927b8d8024e66dc112a09
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-26 14:03:08 +0000

    datatypes-summary

---
+++ 1_basics/datatypes_summary.js
@@ -0,0 +1,29 @@
+// *Primitive*
+// 7 types: String, Number, BigInt, Boolean, Undefined, Symbol,
+//          Null
+
+const Value = 2
+const Valueint = 2.9
+const isLoggedin = true
+const Temp =null
+
+const id = Symbol('232')
+const idiot = Symbol('232')
+console.log(id === idiot)
+const BigNum = 842248948265464436574687644674676349874646
+console.log(typeof(BigNum))
+
+// *Reference (Non-Primitive)*
+//Array, Function, Objects
+
+const fruits = ["apple", "guava", "mango", "cherry"]
+
+let thisObj={
+    name: "Arka",
+    age: 20,
+}
+
+const myFunc = function(){
+    console.log("NIGGA")
+}
+console.log(typeof id)
