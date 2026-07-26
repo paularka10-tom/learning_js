de15bcf4823b07a02bc733993dc9879b8563ba5c
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-26 06:01:02 +0000

    Add conversionOperators.js

---
+++ 1_basics/comparisons.js
@@ -0,0 +1,15 @@
+//console.log(3>8)
+//console.log(7>1)
+
+console.log("2">1)
+console.log("02">1)
+
+//console.log(null==0)  assigns null value to 0
+//console.log(null>=0)
+//console.log(null>0)
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+console.log("2"===2) // strict check, checks datatype
