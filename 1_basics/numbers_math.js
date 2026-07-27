411d703b2e0008cad13044d0f3bbe7b4e8d93e66
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-27 15:09:23 +0000

    numbers_math

--- 1_basics/numbers_math.js
+++ 1_basics/numbers_math.js
@@ -1,4 +1,27 @@
-const x = 200
-console.log(x)
-const bal = new Number(654)
-console.log(bal);
+const x = 2000
+//console.log(x)
+const bal = new Number(6543)
+//console.log(bal)
+
+//console.log(bal.toString().length)
+console.log(bal.toFixed(3))
+
+let goodNumber = 36.54354
+//console.log(goodNumber.toPrecision(5))
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+// MATHS -----   -----  -----  ----  ----  -----  -----
+
+console.log(Math)
+
+//console.log(Math.round(-2.6564))
+//console.log(Math.ceil(5.002))
+//console.log(Math.floor(5.989))
+//console.log(Math.min(5, 7, 9, 4, 2,6,5))
+//console.log(Math.max(5, 7, 9, 4, 2,6,5))
+
+console.log(Math.random()) // 0<value<1
+console.log(Math.random()*10+1)
