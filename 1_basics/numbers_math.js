69af65920258906437d620f6ae54de48cb518af9
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-27 17:12:06 +0000

    numbers_math.js

--- 1_basics/numbers_math.js
+++ 1_basics/numbers_math.js
@@ -24,4 +24,8 @@ console.log(Math)
 //console.log(Math.max(5, 7, 9, 4, 2,6,5))
 
 console.log(Math.random()) // 0<value<1
-console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1))+min)
