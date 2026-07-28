9830fb15345f57238c9c0e1655cc4e68ab0c3a2e
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-28 06:10:48 +0000

    array2

---
+++ 2_basics/array2.js
@@ -0,0 +1,8 @@
+const marvel = ["iron man", "thor", "hulk"]
+const dc = ["batman", "flash", "superman"]
+
+//marvel.push(dc)
+//console.log(marvel[3][2])
+
+const all = marvel.concat(dc)
+console.log(all)
