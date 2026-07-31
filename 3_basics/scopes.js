be1801c8ce330a54a27f6d7d5897a2920ecbc2d0
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-07-31 20:29:40 +0000

    scopes

--- 3_basics/scopes.js
+++ 3_basics/scopes.js
@@ -3,13 +3,44 @@ let a =500                                // global scope
 if(true){                      // block scope
     let a=5
     const b =20
-    console.log(a)
+    //console.log(a)
 }                             //  {}=>scope for functions, if-else, loops
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+//console.log(a)              // a is declared only in if statement
+//console.log(b)              // b ...  ...   ...     ... 
+
+
+function one(){
+    const username ="DER";
+
+    function two(){
+        const website = "YT"
+        //console.log(username);
+    }
+   // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username="s"
+    if (username=="s"){
+        const p = "web";
+        //console.log(username+p)
+    }
+    //console.log(p)
+}
+//console.log(username)
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+console.log(add(445))
+function add(num){
+    return num+1;
 }
 
-console.log(a)              // a is declared only in if statement
-//console.log(b)              // b ...  ...   ...     ... 
+console.log(adding(445))
+const adding = function(num){
+    return num+1;
+}
