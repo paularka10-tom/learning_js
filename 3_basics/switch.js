87d22dbf180d0c9ddf5d3e6112b35f47ab0aa031
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-08-04 20:06:44 +0000

    switch

---
+++ 3_basics/switch.js
@@ -0,0 +1,21 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "march":
+        console.log("Macrch")
+        break;
+    default:
+        break;
+}
