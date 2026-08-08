609d21edbd5fd300b58fbcd8785f7aa12b46c381
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-08-08 17:54:07 +0000

    six

--- 4_iterations/six.js
+++ 4_iterations/six.js
@@ -8,7 +8,14 @@ console.log(values)
 
 const Nums = [1,2,3,4,5,6,7,8]
 
-let hunt = Nums.filter((num)=> num>3)
+// let hunt = Nums.filter((num)=> num>3)
+// console.log(hunt)
 
+// const newNum=[]
 
-console.log(hunt)
+// newNum.forEach( (num)=> {
+//     if(num>4){
+//         newNum.push(num);
+//     }
+// } )
+// console.log(newNum)
