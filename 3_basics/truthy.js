27ef1e265aea624f6bcab5b32f39d1fb10c73a0c
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-08-08 05:39:52 +0000

    tres

--- 3_basics/truthy.js
+++ 3_basics/truthy.js
@@ -19,3 +19,22 @@ if (usermail){
 
  let obj = {}
  if(Object.keys(obj).length===0){console.log("emplty OBJECT")}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val2 = 90
+ //val2 = 1 ?? 4       // for database
+ //val2 = null ?? 19
+
+// val2 =  undefined ?? 8
+
+val2 = null ?? 10 ?? 80
+
+
+console.log(val2)
+
+//Ternary operator
+//condition ? true:false
+
+const water = 90
+water>=60?console.log("more than 60"):console.log("less than 60")
--- 4_iterations/three.js
+++ 4_iterations/three.js
@@ -3,6 +3,37 @@
 
 const kal=[1,2,3,4,5]
 
-for (const num of object) {
-    
+for (const num of kal) {
+    console.log(num);
+}
+
+const greet = "Hello computer"
+for(const i of greet){
+    //console.log(`eachc hcar is ${i}`)
+}
+
+// maps
+const map = new Map()
+map.set('Tengu',90)
+map.set('Moltress',290)
+map.set('Fighter',190)
+map.set('Chomp',50)
+map.set('Fighter',190)
+
+console.log(map)
+
+for (const key of map) {
+    console.log(key)
+}
+for (const [key, value] of map) {
+    console.log(key, `:`+ value)
+}
+
+let newobj = {
+    sport1: 'FOOTBALL',
+    sport2: 'CHESS',
+    sport3: 'VOLLEYBALL'
+}
+for (const [key, value] of newobj) {
+    console.log(key + `:` +vlue)
 }
