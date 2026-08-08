92fb3c0b637836a98bc6236b41d9e546b6212380
Arkaprabha Paul <paularkaprabha10@gmail.com>
2026-08-08 05:02:35 +0000

    do

---
+++ 4_iterations/do.js
@@ -0,0 +1,19 @@
+
+let i = 0;
+while (i<=5) {
+    console.log(`${i}`);
+    i++;
+}
+
+let a1 = ["lemon","aple","mango","peach"]
+let j=0
+while (j< a1.length){
+    console.log(`${a1[j]}`);
+    j++;
+}
+
+let k =1
+do {
+    console.log(`${k}`)
+    k--
+} while (k>2);
